74ef8617dfa68d6176479400010db3649375d9b1
VolkovK04 <112119724+VolkovK04@users.noreply.github.com>
2023-02-26 11:39:18 +0300

    Update player.py

--- classes/player.py
+++ classes/player.py
@@ -1,46 +1,37 @@
 from enum import Enum
-from player import Player
 from point import Point
-from snake import Snake
 
 
-class Cell(Enum):
-    Empty = 0
-    Food = 1
-    Snake = 2
-    Wall = 3
+class Direction(Enum):
+    Up = 0
+    Right = 1
+    Down = 2
+    Left = 3
 
 
-class Game:
-    def __init__(self, map: list[Cell, Cell], players: list[Player]):
-        self.map = map
-        self.players = players
+class Snake:
+    def __init__(self, point: Point, move_direction: Direction) -> None:
+        self.body = [point]
+        self.direction = move_direction
+        self.alive = True
 
-    def next(self):
-        for player in self.players:
-            snake = player.snake
-            new_point = snake.get_next_point()
-            match self.map[new_point.x][new_point.y]:
-                case Cell.Empty:
-                    snake.move()
-                case Cell.Food:
-                    snake.move(is_food=True)
-                case _:
-                    self.delete_snake(snake)
-                    snake.kill()
-
-    def clear_cell(self, point: Point):
-        self.map[point.x][point.y] = Cell.Empty
-
-    def move_snake(self, snake: Snake, is_food=False):
-        next_step = self.get_next_point()
-        self.body.insert(0, next_step)
-        if is_food:
-            self.body.pop()
+    def get_next_point(self):
+        head = self.body[0]
+        match self.direction:
+            case Direction.Up:
+                return Point(head.x, head.y - 1)
+            case Direction.Right:
+                return Point(head.x + 1, head.y)
+            case Direction.Down:
+                return Point(head.x, head.y + 1)
+            case Direction.Left:
+                return Point(head.x - 1, head.y)
+            
+    def change_move(self, new_direction):
+        self.direction = new_direction
 
 
-    def delete_snake(self, snake: Snake):
-        for point in snake.body:
-            self.clear_cell(point)
+    def kill(self):
+        self.alive = False
 
-
+    
