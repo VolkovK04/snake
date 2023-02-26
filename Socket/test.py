75e519f0416ed459efeab3ae93a64d7b1c5ff3d3
VolkovK04 <112119724+VolkovK04@users.noreply.github.com>
2023-02-26 08:49:02 +0300

    Update test.py

--- Socket/test.py
+++ Socket/test.py
@@ -1,34 +1,40 @@
-import socket, asyncio
+from threading import Thread
+import socket
 import time
-import threading
 
 
-def get_conn(serv: socket.socket):
-    return serv.accept()
+MAX_PLAYERS = 10
 
 
-def listen_thread(serv):
-    threading.Thread(target=get_conn, args=(serv, ))
+def get_conn(serv: socket.socket, connections: list, flag=True):
+    while True:
+        connections.append(serv.accept())
 
 
-async def main():
+def main():
     HOST = socket.gethostbyname(socket.gethostname())
     PORT = 9090
- 
+
     print("HOST:", HOST)
     print("PORT:", PORT)
 
     serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serv.bind((HOST, PORT))
 
-    serv.listen(10)
-    timeout = time.time() + 1
+    serv.listen(MAX_PLAYERS)
 
     conns = []
-    while time.time() <= timeout:
-        print(time.time())
-        conns.append(get_conn(serv))
+    flag = True
+    thread = Thread(target=get_conn, args=(serv, conns, flag))
+    thread.start()
+    time.sleep(1)
+    event.set()
+    thread.join()
 
     print(conns)
 
-asyncio.run(main())
+
+main()
+
+
+
