2b45b881e6b43197ffee886bf6a8cc66b54228c9
VolkovK04 <112119724+VolkovK04@users.noreply.github.com>
2023-02-26 11:39:45 +0300

    Update player.py

--- classes/player.py
+++ classes/player.py
@@ -1,37 +1,7 @@
-from enum import Enum
-from point import Point
+from snake import Snake
 
 
-class Direction(Enum):
-    Up = 0
-    Right = 1
-    Down = 2
-    Left = 3
-
-
-class Snake:
-    def __init__(self, point: Point, move_direction: Direction) -> None:
-        self.body = [point]
-        self.direction = move_direction
-        self.alive = True
-
-    def get_next_point(self):
-        head = self.body[0]
-        match self.direction:
-            case Direction.Up:
-                return Point(head.x, head.y - 1)
-            case Direction.Right:
-                return Point(head.x + 1, head.y)
-            case Direction.Down:
-                return Point(head.x, head.y + 1)
-            case Direction.Left:
-                return Point(head.x - 1, head.y)
-            
-    def change_move(self, new_direction):
-        self.direction = new_direction
-
-
-    def kill(self):
-        self.alive = False
-
-    
+class Player:
+    def __init__(self, snake: Snake, id: int):
+        self.snake = snake
+        self.id = id
